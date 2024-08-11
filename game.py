fe9216369f4dc2648b5f7c5207c2a67d47f55e8e
Ahmed A.A. Kh <ahmedakheirallah@gmail.com>
2024-08-11 11:55:59 +0400

    zombies incomplete

--- game.py
+++ game.py
@@ -1,10 +1,13 @@
 import pygame
 from player import Player
+from zombie import Zombie
 class Game:
     def __init__(self):
         self.pygame = pygame
         self.screen = self.pygame.display.set_mode((600, 600))
         self.rect = self.pygame.Rect(275, 275, 50, 50)
+        self.zombie_rect = self.pygame.Rect(0, 0, 50, 50)
+        self.zombies = []
         self.clock = self.pygame.time.Clock()
 
         self.BG = (54, 107, 27)
@@ -24,6 +27,7 @@ class Game:
     def draw(self):
         self.screen.fill(self.BG)
         Player(self.rect).update(self.screen)
+        Zombie(self.zombie_rect, self.zombies).update(self.screen, self.rect)
         self.pygame.display.update()
 
 game = Game()
--- player.py
+++ player.py
@@ -1,12 +1,22 @@
 import pygame
+import math
 class Player:
     def __init__(self, rect):
         self.color = (245, 207, 17)
         self.rect = rect
         self.speed = 5
+        self.image = pygame.transform.scale(pygame.image.load("line.png"), (128, 72))
+        self.image_pos = (236, 264)
 
     def update(self, screen):
         self.movement()
+        mouse_pos = pygame.mouse.get_pos()
+        dx = mouse_pos[0] - self.image_pos[0]
+        dy = mouse_pos[1] - self.image_pos[1]
+        angle = math.degrees(math.atan2(-dy, dx))
+        rotated_image = pygame.transform.rotate(self.image, angle)
+        rotated_rect = rotated_image.get_rect(center=(self.rect.x+25, self.rect.y+25))
+        screen.blit(rotated_image, rotated_rect.topleft)
         pygame.draw.circle(screen, self.color, (self.rect.x+25, self.rect.y+25), self.rect.h/2)
 
     def movement(self):
